95ed1e677b7d2514aeaee2b5d1b15f22a9458410
Christian Pabon <127352132+christian75876@users.noreply.github.com>
2024-04-25 21:20:43 -0500

    Add workshop-5

---
+++ workshop-5/section-1/main.js
@@ -0,0 +1,18 @@
+
+//Objeto numero 1.
+const obj = {
+    nombre: 'jhon',
+    apellido: 'doe',
+    edad: 22,
+    profesion: 'coder',
+    hobbies: 'mecanica',
+};
+
+const saludar = () =>{
+    let msn = `Hola ${obj.nombre} ${obj.apellido}`;
+    return console.log(msn);
+}
+
+saludar();
+
+//Objeto numeor 2.
