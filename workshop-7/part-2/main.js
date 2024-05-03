939dcac1a5f9781881218f7c77749bfbf53d02b3
JuanAmezquitaAgredo <jagredo03@gmail.com>
2024-05-03 14:49:52 -0500

    Add Part-2 Paso-1

---
+++ workshop-7/part-2/main.js
@@ -0,0 +1,37 @@
+function Person(nombre, apellido, edad, profesión, hobbies) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.edad = edad;
+  this.profesión = profesión;
+  this.hobbies = hobbies;
+};
+
+//Metodo de saludar en person:
+Person.prototype.saludar = function () {
+  return `Hola, soy ${this.nombre} ${this.apellido} y tengo ${this.edad} años.`;
+};
+
+//Heredar de Person creando nueva clase Student
+function Student(nombre, apellido, edad, promedio, cursos) {
+  Person.call(this, nombre, apellido, edad);
+  this.promedio = promedio;
+  this.cursos = cursos;
+};
+
+//Hereda todos los prototipos 'Metodos' de la clase padre:
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+let jhonDoe = new Person('Jonh', 'Doe', 28, 'Estudiante', 'Programar');
+console.log(jhonDoe);
+
+console.log(jhonDoe.saludar());
+
+const myStuden = new Student('Juan', 'Giraldo', 22, 'Regular', ['Sprinboot', 'go', 'java']);
+console.log(myStuden);
+
+Student.prototype.presentarExamen = function () {
+  console.log("Estoy presentando un examen.");
+};
+
+myStuden.presentarExamen()
